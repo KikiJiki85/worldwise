b3ef43d2c6bc2d70d72f6ff513c115a1c21d6590
Aleksey Vaskin <alzor85@gmail.com>
2024-09-26 15:21:09 +0300

    feat: create basic project structure with routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const el = document.getElementById("root");
+if (el) {
+  const root = createRoot(el);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import PageNav from '../components/PageNav';
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav></PageNav>
+      <h1>NotFound - 404</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
