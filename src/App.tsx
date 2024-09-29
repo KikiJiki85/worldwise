ec9272393cdf98c971a2043292eb6517d68a7fec
Aleksey Vaskin <alzor85@gmail.com>
2024-09-29 16:32:48 +0300

    feat: add city components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Product from './pages/Product';
@@ -5,8 +6,42 @@ import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout/AppLayout';
 import Login from './pages/Login';
+import CityList from './components/CityList/CityList';
+
+export interface CityType {
+  cityName: string;
+  country: string;
+  emoji: string;
+  date: string;
+  notes: string;
+  id: string;
+  position: {
+    lat: number;
+    lng: number;
+  };
+}
+
+const BASE_URL = 'http://localhost:8000';
 
 function App() {
+  const [cities, setCities] = useState<CityType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, []);
   return (
     <Router>
       <Routes>
@@ -15,7 +50,14 @@ function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/app" element={<AppLayout />}>
-          <Route index path="cities" element={<p>List of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
